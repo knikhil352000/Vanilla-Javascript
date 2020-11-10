84e7189d3a37b837a272ce5c61bfe8a49cbd4694
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-10 21:22:10 +0530

    Added 10 days Functionality

--- CountDown/app.js
+++ CountDown/app.js
@@ -26,7 +26,16 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2020, 10, 24, 17, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+//let futureDate = new Date(2020, 10, 24, 17, 30, 0);
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+
+
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
