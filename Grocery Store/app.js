d322930f4a7b4283e2b7e0979088a1c0fb0f4ae5
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-11 00:42:04 +0530

    Adding item functionality is added

--- Grocery Store/app.js
+++ Grocery Store/app.js
@@ -7,17 +7,50 @@ const container = document.querySelector('.grocery-container');
 const list = document.querySelector('.grocery-list');
 const clearBtn = document.querySelector('.clear-btn');
 
-
 let editElement;
 let editFlag = false;
 let editID = '';
 
-form.addEventListener('submit', )
+form.addEventListener('submit', addItem);
 
-// ****** FUNCTIONS **********
+function addItem(e){
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    if(value !== '' && editFlag === false){
+        const element = document.createElement('article');
+        element.classList.add('grocery-item');
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `<p class="title">${value}</p>
+                            <div class="btn-container">
+                            <button type="button" class="edit-btn"><i class="fas fa-edit"></i></button>
+                            <button type="button" class="delete-btn"><i class="fas fa-trash"></i></button>
+                            </div>`;
+                            //append child
+        list.appendChild(element);
+        displayAlert('item added to list', 'success');
+        //show container
+        container.classList.add('show-container');
+        //add to local storage
+        
+
+    }else if(value !== '' && editFlag === true){
 
 
-
+    }else{
+        displayAlert('please enter value', 'danger')
+    }
+}
+function displayAlert(text, action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+    setTimeout(function(){
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`);
+    },1000);
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
