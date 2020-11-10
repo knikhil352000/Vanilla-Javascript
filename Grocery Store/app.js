548eec515affe6a249cbd5ccba77346400ba2efd
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-10 23:59:05 +0530

    Selected the items in javascript

--- Grocery Store/app.js
+++ Grocery Store/app.js
@@ -1,10 +1,22 @@
-// ****** SELECT ITEMS **********
 
-// edit option
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
-// ****** EVENT LISTENERS **********
+
+let editElement;
+let editFlag = false;
+let editID = '';
+
+form.addEventListener('submit', )
 
 // ****** FUNCTIONS **********
+
+
 
 // ****** LOCAL STORAGE **********
 
