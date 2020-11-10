bae7b6455dbf9a3a403c6011d0e1a28cd766efad
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-10 21:14:55 +0530

    Added Timer in Javascript

--- CountDown/app.js
+++ CountDown/app.js
@@ -21,3 +21,54 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2020, 10, 24, 17, 30, 0);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+let weekday = futureDate.getDay();
+weekday = weekdays[weekday];
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}`;
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime(){
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneHour) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+  const values = [days, hours, minutes, seconds];
+
+  function format(item){
+    if(item < 10){
+      return item = `0${item}`;
+    }else{
+      return item;
+    }
+  }
+
+  items.forEach(function(item, index){
+    item.innerHTML = format(values[index]);
+  })
+  if(t < 0){
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, This giveaway has expired</h4>`
+  }
+}
+
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
+
